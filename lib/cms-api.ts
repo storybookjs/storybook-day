2f4b534ec0cf5149a385c55321a8afb95045f234
winkerVSbecks <varunvachhar@gmail.com>
2022-12-13 10:09:15 -0500

    add OG image and delete unused code

--- lib/cms-api.ts
+++ lib/cms-api.ts
@@ -15,11 +15,6 @@
  */
 import { Job, Sponsor, Stage, Speaker } from '@lib/types';
 
-import * as strapiApi from './cms-providers/strapi';
-import * as agilityApi from './cms-providers/agility';
-import * as datoCmsApi from './cms-providers/dato';
-import * as contentfulApi from './cms-providers/contentful';
-import * as prismicApi from './cms-providers/prismic';
 import * as storyblokApi from './cms-providers/storyblok';
 
 let cmsApi: {
@@ -29,22 +24,8 @@ let cmsApi: {
   getAllJobs: () => Promise<Job[]>;
 };
 
-if (process.env.DATOCMS_READ_ONLY_API_TOKEN) {
-  cmsApi = datoCmsApi;
-} else if (process.env.CONTENTFUL_ACCESS_TOKEN && process.env.CONTENTFUL_SPACE_ID) {
-  cmsApi = contentfulApi;
-} else if (process.env.STORYBLOK_PREVIEW_TOKEN) {
+if (process.env.STORYBLOK_PREVIEW_TOKEN) {
   cmsApi = storyblokApi;
-} else if (process.env.PRISMIC_REPO_ID) {
-  cmsApi = prismicApi;
-} else if (
-  process.env.AGILITY_GUID &&
-  process.env.AGILITY_API_FETCH_KEY &&
-  process.env.AGILITY_API_PREVIEW_KEY
-) {
-  cmsApi = agilityApi;
-} else if (process.env.STRAPI_API_URL) {
-  cmsApi = strapiApi;
 } else {
   cmsApi = {
     getAllSpeakers: () => Promise.resolve([]),
