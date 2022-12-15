1d24bd8fcaf2ba264f2d3919f2f5324d9099abf8
winkerVSbecks <varunvachhar@gmail.com>
2022-12-14 21:43:24 -0500

    wip

--- pages/api/ticket-images/[username].tsx
+++ pages/api/ticket-images/[username].tsx
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-
+import React from 'react';
 import { NextApiRequest, NextApiResponse } from 'next';
 import { SAMPLE_TICKET_NUMBER } from '@lib/constants';
 import { getUserByUsername } from '@lib/db-api';
