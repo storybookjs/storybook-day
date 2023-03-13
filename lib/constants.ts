68c1702e611a9ff00c158fc5ac6e3de50e5cee3a
winkerVSbecks <varunvachhar@gmail.com>
2023-03-13 13:57:58 -0400

    enable registration mode

--- lib/constants.ts
+++ lib/constants.ts
@@ -17,7 +17,7 @@ import { Mode } from './types';
 
 const isDeployPreview = process.env.CONTEXT === 'deploy-preview';
 
-export const MODE: Mode = 'live'; // 'registration'
+export const MODE: Mode = 'registration'; // 'live'
 export const SITE_URL = isDeployPreview
   ? `${process.env.DEPLOY_PRIME_URL}/day`
   : 'https://storybook.js.org/day';
